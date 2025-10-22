0377ede8f0e8f3320a4d9c6cd93e7d85594883e8
Nesma-Osama <Nesma.Ahmed03@eng-st.cu.edu.eg>
2025-10-22 20:42:24 +0300

    finish problme 4

--- Problem Set 1 - Student Version/search.py
+++ Problem Set 1 - Student Version/search.py
@@ -73,8 +73,40 @@ def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
 
 def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     #TODO: ADD YOUR CODE HERE
-    NotImplemented()
-
+    frontier=[]
+    explored=[]
+    counter=0# if there are two path have the same total cost
+    heapq.heappush(frontier,(0,counter,initial_state,[]))# add the initail state with cost =0 and empty path
+    while True:
+        # check if it is not empty
+        if len(frontier)==0:
+            return None
+        # get the first node
+        cost,_,node,path=heapq.heappop(frontier)
+        # add it to explored
+        explored.append(node)
+        # if it is a goal
+        if problem.is_goal(node):
+            return path
+        actions=problem.get_actions(node)
+        for action in actions:
+            successor=problem.get_successor(node,action)
+            # if it is explored ignore it
+            if successor in explored:
+                continue
+            # add the cost of the action
+            new_cost=cost+problem.get_cost(node,action)
+            # if it is not frontier add it
+            if all(successor!=s[2] for s in frontier):
+                counter+=1
+                heapq.heappush(frontier,(new_cost,counter,successor,path+[action]))
+            else:
+                for i in range(len(frontier)):
+                    if frontier[i][2]==successor:
+                        if frontier[i][0]>new_cost:
+                            frontier[i]=(new_cost,counter,successor,path+[action])
+                            heapq.heapify(frontier)
+        
 def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
     #TODO: ADD YOUR CODE HERE
     NotImplemented()
